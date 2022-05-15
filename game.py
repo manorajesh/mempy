6177c8b9607d8fad40c9969fe82d562066cd7910
manorajesh <mano@raje.sh>
2022-05-14 17:49:30 -0700

    Add basic anti-cheat and comments

--- game.py
+++ game.py
@@ -1,31 +1,38 @@
 import readchar as rc
 import random
 import time
+import hashlib
+
+# To remove the high scores file,
+# search for the .scores file.
+# It is hidden as denoted with
+# the dot at the start.  
 
 def saving_highscore(score):
     try:
         file = open(".scores", "r")
-    except FileExistsError:
+    except FileNotFoundError:
         file = file = open(".scores", "w+")
 
     try:
         if int(file.read()) < score:
             print("\nNew Highscore!")
             file = open(".scores", "w")
-            file.write(str(score))
+            file.write(str(score) + "\n" + str(hashlib.sha256(str(score).encode()).hexdigest()))
     except ValueError:
         usrInput = input("Create new .scores file? Will delete file in directory named that (Y/n)")
         if usrInput == "" or "Y" or "y":
             file = open(".scores", "w")
-            file.write(str(score))
+            file.write(str(score) + "\n" + str(hashlib.sha256(str(score).encode()).hexdigest()))
         elif usrInput == "n" or "N":
             print("File not created")
         else:
             file = open(".scores", "w")
-            file.write(str(score))
+            file.write(str(score) + "\n" + str(hashlib.sha256(str(score).encode()).hexdigest()))
     file.close()
     exit()
 
+## Commands can be remapped to user's liking
 LEFT = 'a'
 RIGHT = 'd'
 UP = 'w'
@@ -36,11 +43,15 @@ seed = []
 choice = [UP, DOWN, LEFT, RIGHT]
 score = 0
 
+## High score managing
 try:
     file = open(".scores", "r")
-    high_score = file.read()
-except ValueError:
-    pass
+    high_score, hash = file.read().split()
+    if hash != hashlib.sha256(str(high_score).encode()).hexdigest(): # basic anti-cheat
+        print("This .scores file is not valid. Don't cheat")
+        exit()
+except (ValueError, FileNotFoundError):
+    high_score = "0"
 
 while (keyInput != 'q'):
     print("\033[2J\033[;H", end='') # clear the screen
@@ -64,5 +75,4 @@ while (keyInput != 'q'):
             print(" ".join(seed[:i]) + u" \u001b[41;1m" + keyInput + u"\u001b[0m " + " ".join(seed[i+1:]))
             print(u"\u001b[1000D") # send cursor to left
             print(" " * space + u"\u001b[32;1m" + seed[i] + u"\u001b[0m")
-            file.close()
             saving_highscore(score)
